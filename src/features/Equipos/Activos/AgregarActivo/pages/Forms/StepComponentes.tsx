43e37f6e52a86e5f7562c150c193cc65a9e203fe
kevin523523 <keviprad@espol.edu.ec>
2024-11-07 13:12:03 -0500

    Cambios en el formulario

--- src/features/Equipos/Activos/AgregarActivo/pages/Forms/StepComponentes.tsx
+++ src/features/Equipos/Activos/AgregarActivo/pages/Forms/StepComponentes.tsx
@@ -1,5 +1,258 @@
-import React from "react";
+import { useState } from "react";
+import { useAgregarComponentes } from "../../hooks/useAgregarComponentes";
+import {
+  Marca,
+  Modelo,
+  Serie,
+  Periferico,
+
+} from "../../../../../../types";
+import { Componente } from "../../../../../../types/Activo/Componente/index.ts";
+import { Autocomplete, Button, TextField } from "@mui/material";
+import Icon from "@mui/material/Icon";
+import usePerifericos from "@hooks/usePerifericos";
+import { useSeriesPorModelo } from "@hooks/useSeriesPorModelo";
+import useMarcasPorPeriferico from "@hooks/useMarcasPorPeriferico";
+import { useModelosPorMarcaPeriferico } from "@hooks/useModelosPorMarcaPeriferico";
+
 
 export const StepComponentes = () => {
-  return <div>StepComponentes</div>;
+  const [componentes, setComponentes] = useState<Componente[]>([]);
+  const eliminarComponente = (index: number) => {
+    setComponentes(componentes.filter((_, i) => i !== index));
+  };
+  const [nuevoComponente, setNuevoComponente] = useState<Componente>({
+    periferico: null,
+    marca: null,
+    modelo: null,
+    serie: null,
+    inventario: "",
+  });
+  const limpiarCamposDependientesComponente = () => {
+    setNuevoComponente({
+      ...nuevoComponente,
+      periferico: null,
+      marca: null,
+      modelo: null,
+      serie: null,
+      inventario: "",
+    });
+  };
+  const { marcas: marcasComponente } = useMarcasPorPeriferico(
+    nuevoComponente.periferico?.id_periferico ?? ""
+  );
+  const { perifericos } = usePerifericos();
+  const filteredPerifericos = perifericos.filter(
+    (p) =>
+      p?.nombre.toLowerCase() !== "computadora" &&
+      p?.nombre.toLowerCase() !== "laptop"
+  );
+  const { agregarComponentes } = useAgregarComponentes();
+  const { modelos: modelosComponente } = useModelosPorMarcaPeriferico(
+    nuevoComponente.marca?.id_marca ?? "",
+    nuevoComponente.periferico?.id_periferico ?? ""
+  );
+  const [errorMensajeComponente, setErrorMensajeComponente] = useState<
+  string | null
+>(null);
+  const { series: seriesComponente } = useSeriesPorModelo(
+    nuevoComponente.periferico?.id_periferico ?? "",
+    nuevoComponente.marca?.id_marca ?? "",
+    nuevoComponente.modelo?.id_modelo ?? ""
+  );
+  const handleMarcaComponenteChange = (
+    _event: React.SyntheticEvent<Element, Event>,
+    newValue: Marca | null
+  ) => {
+    setNuevoComponente({
+      ...nuevoComponente,
+      marca: newValue,
+      modelo: null,
+      serie: null,
+    });
+  };
+  const handlePerifericoComponenteChange = (
+    _event: React.SyntheticEvent<Element, Event>,
+    newValue: Periferico | null
+  ) => {
+    setNuevoComponente({
+      ...nuevoComponente,
+      periferico: newValue,
+      marca: null,
+      modelo: null,
+      serie: null,
+    });
+  };
+  const handleModeloComponenteChange = (
+    _event: React.SyntheticEvent<Element, Event>,
+    newValue: Modelo | null
+  ) => {
+    setNuevoComponente({ ...nuevoComponente, modelo: newValue, serie: null });
+  };
+  const handleSerieComponenteChange = (
+    _event: React.SyntheticEvent<Element, Event>,
+    newValue: Serie | null
+  ) => {
+    setNuevoComponente({ ...nuevoComponente, serie: newValue });
+  };
+  const agregarComponente = () => {
+    if (
+      nuevoComponente.periferico &&
+      nuevoComponente.marca &&
+      nuevoComponente.modelo &&
+      nuevoComponente.serie &&
+      nuevoComponente.inventario !== ""
+    ) {
+      setComponentes([...componentes, nuevoComponente]);
+      setNuevoComponente({
+        periferico: {} as Periferico,
+        marca: {} as Marca,
+        modelo: {} as Modelo,
+        serie: {} as Serie,
+        inventario: "",
+      });
+      limpiarCamposDependientesComponente();
+      setErrorMensajeComponente(null);
+    } else {
+      setErrorMensajeComponente(
+        "Por favor, complete todos los campos del componente."
+      );
+    }
+  };
+  return (
+    <div className="mt-8">
+    <div className="flex flex-col md:flex-row gap-4">
+      <div className="flex-1 overflow-x-auto">
+        <table className="min-w-full border-collapse border border-gray-300">
+          <thead>
+            <tr className="bg-gray-100 border-b">
+              <th className="py-2 px-4 border">Periférico</th>
+              <th className="py-2 px-4 border">Marca</th>
+              <th className="py-2 px-4 border">Modelo</th>
+              <th className="py-2 px-4 border">Serie</th>
+              <th className="py-2 px-4 border">Inventario</th>
+              <th className="py-2 px-1 border">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {componentes.map((comp, index) => (
+              <tr key={index}>
+                <td className="py-2 px-4 border">
+                  {comp.periferico?.nombre}
+                </td>
+                <td className="py-2 px-4 border">{comp.marca?.nombre}</td>
+                <td className="py-2 px-4 border">{comp.modelo?.nombre}</td>
+                <td className="py-2 px-4 border">{comp.serie?.nombre}</td>
+                <td className="py-2 px-4 border">{comp.inventario}</td>
+                <td className="py-2 px-1 border">
+                  <Icon
+                    icon="weui:delete-outlined"
+                    width="25"
+                    height="25"
+                    onClick={() => eliminarComponente(index)}
+                    className="cursor-pointer mx-auto"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex-1 space-y-4">
+        <Autocomplete
+          size="small"
+          disablePortal
+          options={filteredPerifericos}
+          getOptionLabel={(option: Periferico) => option?.nombre || ""}
+          onChange={handlePerifericoComponenteChange}
+          value={nuevoComponente.periferico}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Periférico"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+        />
+        <Autocomplete
+          size="small"
+          disablePortal
+          options={marcasComponente}
+          getOptionLabel={(option: Marca) => option?.nombre || ""}
+          onChange={handleMarcaComponenteChange}
+          value={nuevoComponente.marca}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Marca"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+          disabled={!nuevoComponente.periferico}
+        />
+        <Autocomplete
+          size="small"
+          disablePortal
+          options={modelosComponente}
+          getOptionLabel={(option: Modelo) => option?.nombre || ""}
+          onChange={handleModeloComponenteChange}
+          value={nuevoComponente.modelo}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Modelo"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+          disabled={!nuevoComponente.marca}
+        />
+        <Autocomplete
+          size="small"
+          disablePortal
+          options={seriesComponente}
+          getOptionLabel={(option: Serie) => option?.nombre || ""}
+          onChange={handleSerieComponenteChange}
+          value={nuevoComponente.serie}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Serie"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+          disabled={!nuevoComponente.modelo}
+        />
+        <TextField
+          label="Inventario"
+          placeholder="Inventario"
+          variant="outlined"
+          fullWidth
+          size="small"
+          value={nuevoComponente.inventario}
+          onChange={(e) =>
+            setNuevoComponente({
+              ...nuevoComponente,
+              inventario: e.target.value,
+            })
+          }
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={agregarComponente}
+          fullWidth
+        >
+          Agregar Componente
+        </Button>
+        {errorMensajeComponente && (
+          <div className="text-red-500 mb-4">{errorMensajeComponente}</div>
+        )}
+      </div>
+    </div>
+  </div>
+  )
 };
--- src/features/Equipos/Activos/AgregarActivo/pages/Forms/StepInformacionGeneral.tsx
+++ src/features/Equipos/Activos/AgregarActivo/pages/Forms/StepInformacionGeneral.tsx
@@ -1,5 +1,200 @@
-import React from "react";
+import { Autocomplete, Box, TextField } from "@mui/material";
+import useSistemasOperativos from "@hooks/useSistemasOperativos";
+import { useState } from "react";
+import {
+  SistemaOperativo,
+  Dominio,
+  Antivirus,
+  VersionOffice,
+  RAM,
+  Disco,
+  VersionSO
+} from "../../../../../../types";
+import useDominios from "@hooks/useDominios";
+import { antivirus, protocolos } from "../../../../../../data";
+import useVersionesOffice from "@hooks/useVersionesOffice";
+import useRam from "@hooks/useRam";
+import useDiscos from "@hooks/useDiscos";
+import useVersionesSO from "@hooks/useVersionesSO";
 
 export const StepInformacionGeneral = () => {
-  return <div>StepInformacionGeneral</div>;
+  const [selectedSO, setSelectedSO] = useState<SistemaOperativo | null>(null);
+  const { sistemasOperativos } = useSistemasOperativos();
+  const [selectedDominio, setSelectedDominio] = useState<Dominio | null>(null);
+  const { dominios } = useDominios();
+  const [nombreEquipo, setNombreEquipo] = useState<string | null>(null);
+  const [protocolo, setProtocolo] = useState<string | null>(null);
+  const [direccionIP, setDireccionIP] = useState<string>("");
+  const [selectedAntivirus, setSelectedAntivirus] = useState<Antivirus | null>(
+    null
+  );
+  const [selectedDisco, setSelectedDisco] = useState<Disco | null>(null);
+  const { versionesOffice } = useVersionesOffice();
+  const [selectedVersionOffice, setSelectedVersionOffice] =
+    useState<VersionOffice | null>(null);
+  const { ram } = useRam();
+  const [selectedRAM, setSelectedRAM] = useState<RAM | null>(null);
+  const { versionesSO } = useVersionesSO(selectedSO?.id_sistemaoperativo ?? "");
+  const [selectedVersionSO, setSelectedVersionSO] = useState<VersionSO | null>(
+    null
+  );
+  const { discos } = useDiscos();
+  return (
+    <Box>
+      <div className="mt-8">
+        <div className="grid grid-cols-2 gap-4">
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={sistemasOperativos}
+            getOptionLabel={(option) => option.nombre}
+            value={selectedSO}
+            onChange={(_, newValue) => setSelectedSO(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Sistema Operativo"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={versionesSO}
+            getOptionLabel={(option) => option.nombre}
+            value={selectedVersionSO}
+            onChange={(_, newValue) => setSelectedVersionSO(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Versión SO"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={dominios}
+            getOptionLabel={(option) => option.nombre}
+            value={selectedDominio}
+            onChange={(_, newValue) => setSelectedDominio(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Dominio"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <TextField
+            label="Nombre Equipo"
+            placeholder="Nombre Equipo"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={nombreEquipo}
+            onChange={(e) => setNombreEquipo(e.target.value)}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={versionesOffice}
+            getOptionLabel={(option) => option.nombre}
+            value={selectedVersionOffice}
+            onChange={(_, newValue) => setSelectedVersionOffice(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Versión Office"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={protocolos}
+            getOptionLabel={(option) => option.nombre}
+            value={protocolos.find((p) => p.id === protocolo) || null}
+            onChange={(event, newValue) => {
+              if (newValue) {
+                setProtocolo(newValue.id);
+              } else {
+                setProtocolo("1");
+              }
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Protocolo"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <TextField
+            label="Dirección IP"
+            placeholder="Dirección IP"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={direccionIP}
+            onChange={(e) => setDireccionIP(e.target.value)}
+            disabled={protocolo !== "0"}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={antivirus}
+            getOptionLabel={(option) => option.nombre}
+            value={selectedAntivirus}
+            onChange={(_, newValue) => setSelectedAntivirus(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Antivirus"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={ram}
+            getOptionLabel={(option: RAM) =>
+              `${option.capacidad} - ${option.tipo}`
+            }
+            value={selectedRAM}
+            onChange={(_, newValue) => setSelectedRAM(newValue)}
+            renderInput={(params) => (
+              <TextField {...params} label="RAM" variant="outlined" fullWidth />
+            )}
+          />
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={discos}
+            getOptionLabel={(option) => option.capacidad}
+            value={selectedDisco}
+            onChange={(_, newValue) => setSelectedDisco(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Disco"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          />
+        </div>
+      </div>
+    </Box>
+  );
 };
