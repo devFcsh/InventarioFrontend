8f7f42e8c844f21178a742ac64fb10b90e67125d
kevin523523 <89325741+kevin523523@users.noreply.github.com>
2025-07-18 07:59:13 -0500

    Update EditarActivoSimple.tsx

--- src/features/Equipos/Activos/EditarActivo/pages/EditarActivoSimple.tsx
+++ src/features/Equipos/Activos/EditarActivo/pages/EditarActivoSimple.tsx
@@ -99,7 +99,7 @@ const EditarActivoSimple = ({
       setSelectedInventarioInv(equipoSimpleActivo.inventario);
       setCurrentImagePath(equipoSimpleActivo.imagenRuta);
       setNewObservation(equipoSimpleActivo.observacion);
-      (equipo.inventario.length === 10 || equipo.inventario.length === 12)
+      (equipoSimpleActivo.inventario.length === 10 || equipoSimpleActivo.inventario.length === 12)
         ? setEmpresa("EspolTech")
         : setEmpresa("Espol");
     }
