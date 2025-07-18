5e2778818500bba9e904949ba8029d4b4d2b120b
kevin523523 <89325741+kevin523523@users.noreply.github.com>
2025-07-18 07:45:24 -0500

    Permitir 6 y 8 para espol y para espoltech 10 y 12 caracteres

--- src/pages/Forms/helpers/validateInventario.ts
+++ src/pages/Forms/helpers/validateInventario.ts
@@ -1,12 +1,12 @@
 export const validateInventario = (value: any, empresa: string) => {
   value = value.trim();
   
-  const regexPattern = /^[\d-]+$/;
+  const regexPattern = /^[\d-]*$/;
   
   if (regexPattern.test(value)) {
-    if (empresa === "Espol" && value.length === 8) {
+    if (empresa === "Espol" && (value.length === 6 || value.length === 8)) {
       return true;
-    } else if (empresa === "EspolTech" && value.length === 12) {
+    } else if (empresa === "EspolTech" && (value.length === 10 || value.length === 12)) {
       return true;
     } else {
       return false;
