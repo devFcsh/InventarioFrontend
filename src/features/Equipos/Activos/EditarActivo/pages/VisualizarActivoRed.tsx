2b6f47a3bbcf9633ab75ff6baa5d26d6dd7f9c0e
kevin523523 <89325741+kevin523523@users.noreply.github.com>
2025-07-18 07:54:37 -0500

    Update VisualizarActivoRed.tsx

--- src/features/Equipos/Activos/EditarActivo/pages/VisualizarActivoRed.tsx
+++ src/features/Equipos/Activos/EditarActivo/pages/VisualizarActivoRed.tsx
@@ -59,7 +59,7 @@ const VisualizarActivoRed = ({
       setSelectedPuertos(equipoRedActivo.puertos);
       setSelectedPuertoFTP(equipoRedActivo.puerto_ftp);
       setNombreEquipo(equipoRedActivo.nombre_equipo ?? "");
-      equipoRedActivo.inventario.length === 10
+      (equipo.inventario.length === 10 || equipo.inventario.length === 12)
         ? setEmpresa("EspolTech")
         : setEmpresa("Espol");
     }
