bf03631ebfaba6a2fe0df9edc15b8724151774b5
jairrami06 <jairrami@espol.edu.ec>
2025-08-28 12:51:15 -0500

    Cambios para importar

--- src/features/Equipos/Activos/Homepage/Activos.tsx
+++ src/features/Equipos/Activos/Homepage/Activos.tsx
@@ -197,7 +197,7 @@ const Activos = () => {
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
+      console.log(jsonData)
       try {
         const equiposImport = jsonData.map(transformarFilaExcel);
 
