52fb516a1726fed215072e6fa2276e916a87ff45
jairrami06 <jairrami@espol.edu.ec>
2025-09-11 10:19:18 -0500

    feat: refactor visualizar menu

--- src/features/Equipos/Activos/EditarActivo/pages/VisualizarComputadoraActivo.tsx
+++ src/features/Equipos/Activos/EditarActivo/pages/VisualizarComputadoraActivo.tsx
@@ -593,7 +593,13 @@ const VisualizarComputadoraActivo = ({
                       <td className="py-2 px-4 border">
                         {comp.modelo?.nombre}
                       </td>
-                      <td className="py-2 px-4 border">{comp.serie}</td>
+                      <td className="py-2 px-4 border">
+                        {typeof comp.serie === "object" &&
+                        comp.serie !== null &&
+                        "nombre" in comp.serie
+                          ? comp.serie.nombre
+                          : comp.serie}
+                      </td>
                       <td className="py-2 px-4 border">{comp.inventario}</td>
                     </tr>
                   ))
